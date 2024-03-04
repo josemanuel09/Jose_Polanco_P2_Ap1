701ea6b6577784f1b921d84a8a3f13f3b4c261f7
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-04 10:24:28 -0400

    Creando el contexto

---
+++ Jose_Polanco_P2_Ap1.Api/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Jose_Polanco_P2_Ap1.Api.DAL
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options):base(options)
+        {
+        }
+    }
+}
