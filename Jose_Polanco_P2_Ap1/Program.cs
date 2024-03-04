7fc50beaba337a980825c65b60dfc8eb0ded39e9
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-04 09:51:16 -0400

    Add project files.

---
+++ Jose_Polanco_P2_Ap1/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Jose_Polanco_P2_Ap1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
