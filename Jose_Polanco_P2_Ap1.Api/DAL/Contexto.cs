2f45fdc56917ffdda86be852b25371de0e6754f8
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-04 20:48:12 -0400

    Creando el contexto

--- Jose_Polanco_P2_Ap1.Api/DAL/Contexto.cs
+++ Jose_Polanco_P2_Ap1.Api/DAL/Contexto.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Jose_Polanco_P2_Ap1.Shared.Models;
+
 
 namespace Jose_Polanco_P2_Ap1.Api.DAL
 {
@@ -6,6 +8,26 @@ namespace Jose_Polanco_P2_Ap1.Api.DAL
     {
         public Contexto(DbContextOptions<Contexto> options):base(options)
         {
+
         }
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+        public DbSet<Accesorios> Accesorios { get; set; }
+        public DbSet<VehiculosDetalle> VehiculosDetalle { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Accesorios>().HasData(new List<Accesorios>()
+            {
+                new Accesorios() { AccesorioId = 1, Descripcion = "Camara Trasera" },
+                new Accesorios() { AccesorioId = 2, Descripcion =  "Pantalla Interior"},
+                new Accesorios() { AccesorioId = 3, Descripcion= "Interior en piel" },
+                new Accesorios() { AccesorioId = 4, Descripcion = "Sun Roof" },
+                new Accesorios() { AccesorioId = 5, Descripcion = "Aros de Lujo" }
+               
+            });
+
+
+        }
+
     }
 }
