baa9cfd54cd88a25572b2231eb841831b0335730
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-04 20:48:28 -0400

    Creando migracion

---
+++ Jose_Polanco_P2_Ap1.Api/Migrations/20240305002521_Inicial.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Jose_Polanco_P2_Ap1.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accesorios",
+                columns: table => new
+                {
+                    AccesorioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accesorios", x => x.AccesorioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculos",
+                columns: table => new
+                {
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Costo = table.Column<double>(type: "REAL", nullable: false),
+                    Gastos = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculos", x => x.VehiculoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehiculosDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AccesorioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Valor = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiculosDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VehiculosDetalle_Vehiculos_VehiculoId",
+                        column: x => x.VehiculoId,
+                        principalTable: "Vehiculos",
+                        principalColumn: "VehiculoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accesorios",
+                columns: new[] { "AccesorioId", "Descripcion" },
+                values: new object[,]
+                {
+                    { 1, "Camara Trasera" },
+                    { 2, "Pantalla Interior" },
+                    { 3, "Interior en piel" },
+                    { 4, "Sun Roof" },
+                    { 5, "Aros de Lujo" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehiculosDetalle_VehiculoId",
+                table: "VehiculosDetalle",
+                column: "VehiculoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accesorios");
+
+            migrationBuilder.DropTable(
+                name: "VehiculosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculos");
+        }
+    }
+}
