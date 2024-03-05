448861193c0120c07420f250fa8827b7cc6cf162
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-04 20:47:47 -0400

    Modelo vehiculosDetalle

---
+++ Jose_Polanco_P2_Ap1.Shared/Models/VehiculosDetalle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jose_Polanco_P2_Ap1.Shared.Models
+{
+    public class VehiculosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [ForeignKey("VehiculoId")]
+        public int VehiculoId { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [ForeignKey("AccesorioId")]
+        public int AccesorioId { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public double Valor { get; set; }
+
+
+    }
+}
