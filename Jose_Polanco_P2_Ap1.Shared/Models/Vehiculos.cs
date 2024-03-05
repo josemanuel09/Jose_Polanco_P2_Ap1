2821d04eee9a325d150036a347cfc12d6f258860
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-04 20:47:35 -0400

    Creando modelo de vehiculos

---
+++ Jose_Polanco_P2_Ap1.Shared/Models/Vehiculos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jose_Polanco_P2_Ap1.Shared.Models
+{
+    public class Vehiculos
+    {
+        [Key]
+        public int VehiculoId { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public double Costo { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public double Gastos { get; set; }
+        [ForeignKey("VehiculoId")]
+        public ICollection<VehiculosDetalle> VehiculosDetalle { get; set; } = new List<VehiculosDetalle>();
+
+    }
+}
