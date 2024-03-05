f9cb14a5e1a59ba6dc68f697a07e128a8aa15ea3
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-04 20:47:21 -0400

    Creando modelo de accesorios

---
+++ Jose_Polanco_P2_Ap1.Shared/Models/Accesorios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jose_Polanco_P2_Ap1.Shared.Models
+{
+    public class Accesorios
+    {
+        [Key]
+        public int AccesorioId { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Descripcion { get; set; }
+    }
+}
